bb5ea1e8380ce3a300f6829495f949f5164231ed
safinhr <hrsafin2434@gmail.com>
2024-01-23 19:25:46 +0600

    axios refresher:get data using axios

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,24 @@
+import GetData from "./Component/GetData/GetData";
 import Form from "./TwoWayBinding/form"
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+
+  // const router = new createBrowserRouter([
+  //   {
+  //     path : "/",
+
+  //   }
+  // ])
   
 
   return (
     <>
-      <Form />
+      {/* <Form /> */}
+      <GetData />
     </>
   )
 }
---
+++ my-project/src/Component/GetData/GetData.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+const GetData = () => {
+
+    const [user, setUser] = useState([])
+
+    const getUser = async () => {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const data = await res.data
+        setUser(data)
+    }
+    return (
+        <div className=' p-10'>
+            <button onClick={getUser} className='bg-blue-500 rounded-md px-4 py-2 text-white'>Get Data</button>
+            
+            <div className=' bg-gray-50 p-5 mt-6'>
+                 {user.map((item,idx) =><h2 className=' text-lg pt-5' key={idx}>{item.name}</h2>)}
+            </div>
+        </div>
+    );
+};
+
+export default GetData;
