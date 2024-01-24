03be9fe4931ed71fd73ab695fd67067a5a93c3fb
safinhr <hrsafin2434@gmail.com>
2024-01-24 23:30:07 +0600

    card desgin using grid

--- my-project/src/Component/Card/Card.jsx
+++ my-project/src/Component/Card/Card.jsx
@@ -3,22 +3,26 @@ import React from "react";
 const Card = () => {
   return (
     //parent
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10 items-center justify-center p-10">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 items-center justify-center p-10">
+      {/* firs card */}
+      <div className="col-span-2 bg-red-600">
+        <div className="card   w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img
